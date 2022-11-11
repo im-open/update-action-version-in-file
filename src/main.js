0a4cacf38833df745a76507e19a348aee79b048d
Danielle Adams <danielle.adams3@extendhealth.com>
2022-11-11 15:58:41 -0700

    ARCH-1617 - Implementing the initial action

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-const core = require('@actions/core'); // TODO:  Run `npm install @actions/core` to get latest version
-// const github = require('@actions/github');  // TODO:  run an npm install for this if using
+const core = require('@actions/core');
 
 // When used, this requiredArgOptions will cause the action to error if a value has not been provided.
 const requiredArgOptions = {
@@ -7,5 +6,26 @@ const requiredArgOptions = {
   trimWhitespace: true
 };
 
-//const token = core.getInput('github-token', requiredArgOptions);
-//const octokit = github.getOctokit(token);
+const fs = require('fs');
+
+const fileName = core.getInput('file-to-update', requiredArgOptions);
+const actionName = core.getInput('action-name', requiredArgOptions);
+
+const versionPrefixInput = core.getInput('version-prefix', requiredArgOptions);
+const versionPrefix = versionPrefixInput == 'none' ? '' : versionPrefixInput;
+
+const updatedVersion = core.getInput('updated-version', requiredArgOptions);
+const saveFile = core.getBooleanInput('save-file', requiredArgOptions);
+
+const rawFileContent = fs.readFileSync(fileName, 'utf8');
+const regexString = `${actionName}@${versionPrefix}([0-9]+)(\.[0-9]+)*(\.[0-9]+)*`;
+const actionVersionRegex = new RegExp(regexString, 'ig');
+
+const replacementValue = `${actionName}@${updatedVersion}`;
+
+const updatedFileContent = rawFileContent.replace(actionVersionRegex, replacementValue);
+if (saveFile) {
+  fs.writeFileSync(fileName, updatedFileContent);
+}
+
+core.setOutput('updated-content', updatedFileContent);
